1cf9ad004ab368f0c1f26686a276e3a1fd6c38bc
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-23 13:09:02 +0300

    Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    let newPosition = left + 40;
+  
+    if (newPosition > 360) {
+      newPosition = 360;
+    }
+        dodger.style.left = `${newPosition}px`;
+      }
